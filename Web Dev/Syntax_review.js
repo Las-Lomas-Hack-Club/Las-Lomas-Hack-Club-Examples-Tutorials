fa8a40b1e1be9610e3b4b4b2bae007a97ba63488
Evan Nishi <42789870+pErs0nZ@users.noreply.github.com>
2019-04-22 16:52:37 -0700

    Update Syntax_review.js

--- Web Dev/Syntax_review.js
+++ Web Dev/Syntax_review.js
@@ -11,12 +11,12 @@ console.log("js is good");
 alert("js is good")  //this will give an error if not enclosed in <script> tags on an html webpage
 // let is local if in function but global if outside
 let test1; //can also declare like let test1 = 5;
-test1 = 5
+test1 = 5;
 //var is always global
 var test2; //can also declare lkie let test2 = 6;
-test2 = 6
+test2 = 6;
 //constants cannot be changed or you get error
-const test3 = 7 // cannot do const test3 
+const test3 = 7; // cannot do const test3 
                 //test3 = 7
 //js data types don't have to be specified explicitly
 var mystring = "hello"; //string
@@ -26,6 +26,8 @@ var mynull = null; //null or nonetype
 var myundef; //if you don't declare val of variable it is just undefined
 
 //if you are unsure of type use
-///console.log(typeof variable_name) returns type of obj
+console.log(typeof variable_name); //returns type of obj
 
-//backticks are a pain but lez do this
+//If you are on an html website, you can ask the user to input things with
+var myvar = propt("my prompt");//will give you error if on console
+//User input will be stored in a variable named myvar
