a8a4411673f2f199f3a43b096bab6fb0e451cae9
Evan Nishi <42789870+pErs0nZ@users.noreply.github.com>
2019-04-24 10:35:14 -0700

    Update Syntax_review.js

--- Web Dev/Syntax_review.js
+++ Web Dev/Syntax_review.js
@@ -1,6 +1,8 @@
 /*Note: This is a review, it assumes you already know js and need a refresher.
 This is NOT to learn js the first time, and assumes you can program in js.*/ 
 
+//1. Basics 
+
 //use strict makes it so that the computer uses modern js and is no longer compatable with old js
 //anything below use strict is modern js, use strict cannot be cancelled
 "use strict"
@@ -29,5 +31,16 @@ var myundef; //if you don't declare val of variable it is just undefined
 console.log(typeof variable_name); //returns type of obj
 
 //If you are on an html website, you can ask the user to input things with
-var myvar = propt("my prompt");//will give you error if on console
+var myvar = prompt("my prompt");//will give you error if on console, must be inside an html page
 //User input will be stored in a variable named myvar
+alert(myvar); // will print out on HTML page what the user input
+
+//2. Logic and control flow
+var age = 55; 
+if(age > 14){  //this is a basic if statement, if the condition is true, which it is because 55>14, the code within the {} brackets will be executed
+  console.log("You're so old!");
+}else if (age < 14){  //will only execute if this statement is true, allows to stack mulitple logical operators
+  console.log("small child, you have much to learn")  //code will not execute unless age is less than 14
+}else{ //the code within this statement will only execute if age>14 is false, it will not execute in this case
+  console.log("wow, your as old as me!"); 
+}
