df2ee44bf46214ca3f031d87790c65c28966ef69
Evan Nishi <42789870+pErs0nZ@users.noreply.github.com>
2019-04-26 12:15:29 -0700

    Update C_example.c

--- C and C++/C_example.c
+++ C and C++/C_example.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+//Currency translator in C
 
+//Made this in Sept 2018 but never posted it...
 int main(void)
 {
+    //NEVER do this, if you name variables as badly as I did in this example I will be very dissapointed
     char c;
     printf("Hello, are here to tranlate currency?  Well you are now.  Press enter : ");
-    scanf("%c",&c);
+    //input equiv in C
+    scanf("%c",&);
     while (c != "y")
     {
      float a;
      printf("Enter USD to get value in yuan:$");
      scanf("%f", &a);
      {
+         //These symbols are NOT all unicode, so this may or may not cause errors, :P
          printf("$%f US Dollars = %f Chinese Yuan\n",a, (a) * 6.83);
      }
      float b;
@@ -29,4 +34,8 @@ int main(void)
 }
     return 0;
 }
-
+/*
+This is probably the worst code I've written, but C is pretty cool.  It's very versatile, and you can do high and
+low level programming with it (even though high level stuff is a total pain).
+Well, have fun, and keep on coding kids... Wow, I sound weird rn byeee!
+*/
